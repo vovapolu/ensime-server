d1cfde81152ab3ef440d833e7a89da624b8e0167
Sam Halliday <sam.halliday@gmail.com>
2016-02-21 23:28:31 +0000

    FQNs with double dollars are of no value now

--- core/src/main/scala/org/ensime/indexer/SearchService.scala
+++ core/src/main/scala/org/ensime/indexer/SearchService.scala
@@ -175,7 +175,7 @@ class SearchService(
   }
 
   private val blacklist = Set("sun/", "sunw/", "com/sun/")
-  private val ignore = Set("$$anon$", "$$anonfun$", "$worker$")
+  private val ignore = Set("$$", "$worker$")
   import org.ensime.util.RichFileObject._
   private def extractSymbols(container: FileObject, f: FileObject): List[FqnSymbol] = {
     f.pathWithinArchive match {
