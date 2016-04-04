3657d0c569067fd7eede5f5865c61c1ccfa4bf6a
Damian Pawlowski <profes82@gmail.com>
2016-04-04 23:09:27 +0200

    added more assertions

--- core/src/test/scala/org/ensime/indexer/ClassfileIndexerSpec.scala
+++ core/src/test/scala/org/ensime/indexer/ClassfileIndexerSpec.scala
@@ -7,13 +7,42 @@ import org.ensime.fixture.IsolatedEnsimeVFSFixture
 import org.ensime.util.EnsimeSpec
 import org.ensime.vfs._
 
+import scala.collection.immutable.Queue
+
 class ClassfileIndexerSpec extends EnsimeSpec with IsolatedEnsimeVFSFixture {
 
   val indexer = new ClassfileIndexer with SLF4JLogging {}
   import indexer._
 
   "ClassfileIndexer" should "support Java 6 class files" in withVFS { implicit vfs =>
-    indexClassfile(vfs.vres("jdk6/Test.class"))
+    val (clazz, refs) = indexClassfile(vfs.vres("jdk6/Test.class"))
+    clazz.name shouldBe ClassName(PackageName(List()), "Test")
+    clazz.generics shouldBe None
+    clazz.superClass shouldBe Some(ClassName(PackageName(List("java", "lang")), "Object"))
+    clazz.interfaces shouldBe List()
+    clazz.access shouldBe Default
+    clazz.deprecated shouldBe false
+    clazz.fields shouldBe Queue()
+    clazz.methods shouldBe Queue(
+      RawMethod(
+        name = MemberName(ClassName(PackageName(List()), "Test"), "main"),
+        access = Public,
+        descriptor = Descriptor(List(ArrayDescriptor(ClassName(PackageName(List("java", "lang")), "String"))), ClassName(PackageName(List()), "void")),
+        generics = None,
+        line = Some(4)
+      )
+    )
+    clazz.source shouldBe RawSource(Some("Test.java"), Some(1))
+
+    refs shouldBe Set(
+      ClassName(PackageName(List()), "void"),
+      ClassName(PackageName(List("java", "lang")), "Object"),
+      MemberName(ClassName(PackageName(List("java", "lang")), "System"), "out"),
+      ClassName(PackageName(List("java", "lang")), "Object"),
+      ClassName(PackageName(List("java", "io")), "PrintStream"),
+      MemberName(ClassName(PackageName(List("java", "io")), "PrintStream"), "print"),
+      ClassName(PackageName(List("java", "lang")), "String")
+    )
   }
 
   it should "support Java 8 class files" in withVFS { implicit vfs =>
@@ -25,10 +54,16 @@ class ClassfileIndexerSpec extends EnsimeSpec with IsolatedEnsimeVFSFixture {
   it should "support typical J2SE classes" in withVFS { implicit vfs =>
     val (clazz, refs) = indexClassfile(vfs.vres("java/lang/String.class"))
     clazz.access shouldBe Public
+    clazz.name shouldBe ClassName(PackageName(List("java", "lang")), "String")
   }
 
   it should "support typical Scala classes" in withVFS { implicit vfs =>
-    indexClassfile(vfs.vres("scala/collection/immutable/List.class"))
-    indexClassfile(vfs.vres("scala/collection/immutable/List$.class"))
+    val (clazz, refs) = indexClassfile(vfs.vres("scala/collection/immutable/List.class"))
+    clazz.name shouldBe ClassName(PackageName(List("scala", "collection", "immutable")), "List")
+  }
+
+  it should "support typical Scala nested classes " in withVFS { implicit vfs =>
+    val (clazz, refs) = indexClassfile(vfs.vres("scala/collection/immutable/List$.class"))
+    clazz.name shouldBe ClassName(PackageName(List("scala", "collection", "immutable")), "List$")
   }
 }
