48a6d824386576186b89013ff3cfa4017e3aa79b
Sam Halliday <sam.halliday@gmail.com>
2016-06-01 23:06:32 +0100

    it was the best of times it was the worst of times

--- core/src/it/scala/org/ensime/intg/BasicWorkflow.scala
+++ core/src/it/scala/org/ensime/intg/BasicWorkflow.scala
@@ -90,11 +90,12 @@ class BasicWorkflow extends EnsimeSpec
           project ! PublicSymbolSearchReq(List("scala", "util", "Random"), 2)
           expectMsgPF() {
             case SymbolSearchResults(List(
-              TypeSearchResult("scala.util.Random", "Random", DeclaredAs.Class, Some(_)),
-              TypeSearchResult("scala.util.Random$", "Random$", DeclaredAs.Class, Some(_)))) =>
-            case SymbolSearchResults(List(
-              TypeSearchResult("java.util.Random", "Random", DeclaredAs.Class, Some(_)),
-              TypeSearchResult("scala.util.Random", "Random", DeclaredAs.Class, Some(_)))) =>
+              TypeSearchResult(full1, short1, DeclaredAs.Class, Some(_)),
+              TypeSearchResult(full2, short2, DeclaredAs.Class, Some(_))
+              )) if full1.contains("Random") &&
+              short1.contains("Random") &&
+              full2.contains("Random") &&
+              short2.contains("Random") =>
             // this is a pretty ropey test at the best of times
           }
 
