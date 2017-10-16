7548609e6f62b33415d5ace7e19128b149e5866f
Sam Halliday <sam.halliday@gmail.com>
2017-10-16 08:07:26 +0100

    remove implicit object from json (#1883)

--- json/src/main/scala/spray/json/BasicFormats.scala
+++ json/src/main/scala/spray/json/BasicFormats.scala
@@ -7,126 +7,89 @@ package spray.json
  */
 trait BasicFormats {
 
-  implicit object IntJsonFormat extends JsonFormat[Int] {
-    def write(x: Int) = JsNumber(x)
-    def read(value: JsValue) = value match {
-      case JsNumber(x) => x.intValue
-      case x           => deserializationError("Expected Int as JsNumber, but got " + x)
-    }
+  implicit val int: JsonFormat[Int] = JsonFormat.instance[Int](JsNumber(_)) {
+    case JsNumber(x) => x.intValue
+    case x           => deserializationError("Expected Int as JsNumber, but got " + x)
   }
 
-  implicit object LongJsonFormat extends JsonFormat[Long] {
-    def write(x: Long) = JsNumber(x)
-    def read(value: JsValue) = value match {
-      case JsNumber(x) => x.longValue
-      case x           => deserializationError("Expected Long as JsNumber, but got " + x)
-    }
+  implicit val long: JsonFormat[Long] = JsonFormat.instance[Long](JsNumber(_)) {
+    case JsNumber(x) => x.longValue
+    case x           => deserializationError("Expected Long as JsNumber, but got " + x)
   }
 
-  implicit object FloatJsonFormat extends JsonFormat[Float] {
-    def write(x: Float) = JsNumber(x.toDouble)
-    def read(value: JsValue) = value match {
+  implicit val float: JsonFormat[Float] =
+    JsonFormat.instance[Float](f => JsNumber(f.toDouble)) {
       case JsNumber(x) => x.floatValue
       case JsNull      => Float.NaN
       case x           => deserializationError("Expected Float as JsNumber, but got " + x)
     }
-  }
 
-  implicit object DoubleJsonFormat extends JsonFormat[Double] {
-    def write(x: Double) = JsNumber(x)
-    def read(value: JsValue) = value match {
+  implicit val double: JsonFormat[Double] =
+    JsonFormat.instance[Double](JsNumber(_)) {
       case JsNumber(x) => x.doubleValue
       case JsNull      => Double.NaN
       case x =>
         deserializationError("Expected Double as JsNumber, but got " + x)
     }
-  }
 
-  implicit object ByteJsonFormat extends JsonFormat[Byte] {
-    def write(x: Byte) = JsNumber(x.toInt)
-    def read(value: JsValue) = value match {
+  implicit val byte: JsonFormat[Byte] =
+    JsonFormat.instance[Byte](b => JsNumber(b.toInt)) {
       case JsNumber(x) => x.byteValue
       case x           => deserializationError("Expected Byte as JsNumber, but got " + x)
     }
-  }
 
-  implicit object ShortJsonFormat extends JsonFormat[Short] {
-    def write(x: Short) = JsNumber(x.toInt)
-    def read(value: JsValue) = value match {
+  implicit val short: JsonFormat[Short] =
+    JsonFormat.instance[Short](s => JsNumber(s.toInt)) {
       case JsNumber(x) => x.shortValue
       case x           => deserializationError("Expected Short as JsNumber, but got " + x)
     }
-  }
 
-  implicit object BigDecimalJsonFormat extends JsonFormat[BigDecimal] {
-    def write(x: BigDecimal) = {
-      require(x ne null)
-      JsNumber(x)
-    }
-    def read(value: JsValue) = value match {
+  implicit val bigDecimal: JsonFormat[BigDecimal] =
+    JsonFormat.instance[BigDecimal](JsNumber(_)) {
       case JsNumber(x) => x
       case JsString(x) => BigDecimal(x)
       case x =>
         deserializationError("Expected BigDecimal as JsNumber, but got " + x)
     }
-  }
 
-  implicit object BigIntJsonFormat extends JsonFormat[BigInt] {
-    def write(x: BigInt) = {
-      require(x ne null)
-      JsNumber(x)
-    }
-    def read(value: JsValue) = value match {
+  implicit val bigInt: JsonFormat[BigInt] =
+    JsonFormat.instance[BigInt](JsNumber(_)) {
       case JsNumber(x) => x.toBigInt
       case JsString(x) => BigInt(x)
       case x =>
         deserializationError("Expected BigInt as JsNumber, but got " + x)
     }
-  }
 
-  implicit object UnitJsonFormat extends JsonFormat[Unit] {
-    def write(x: Unit)             = JsNumber(1)
-    def read(value: JsValue): Unit = {}
-  }
+  implicit val unit: JsonFormat[Unit] =
+    JsonFormat.instance[Unit](_ => JsNumber(1))(_ => ())
 
-  implicit object BooleanJsonFormat extends JsonFormat[Boolean] {
-    def write(x: Boolean) = JsBoolean(x)
-    def read(value: JsValue) = value match {
+  implicit val boolean: JsonFormat[Boolean] =
+    JsonFormat.instance[Boolean](JsBoolean(_)) {
       case JsTrue  => true
       case JsFalse => false
       case x       => deserializationError("Expected JsBoolean, but got " + x)
     }
-  }
 
-  implicit object CharJsonFormat extends JsonFormat[Char] {
-    def write(x: Char) = JsString(String.valueOf(x))
-    def read(value: JsValue) = value match {
+  implicit val char: JsonFormat[Char] =
+    JsonFormat.instance[Char](c => JsString(String.valueOf(c))) {
       case JsString(x) if x.length == 1 => x.charAt(0)
       case x =>
         deserializationError(
           "Expected Char as single-character JsString, but got " + x
         )
     }
-  }
 
-  implicit object StringJsonFormat extends JsonFormat[String] {
-    def write(x: String) = {
-      require(x ne null)
-      JsString(x)
-    }
-    def read(value: JsValue) = value match {
+  implicit val string: JsonFormat[String] =
+    JsonFormat.instance[String](JsString(_)) {
       case JsString(x) => x
       case x =>
         deserializationError("Expected String as JsString, but got " + x)
     }
-  }
 
-  implicit object SymbolJsonFormat extends JsonFormat[Symbol] {
-    def write(x: Symbol) = JsString(x.name)
-    def read(value: JsValue) = value match {
+  implicit val symbol: JsonFormat[Symbol] =
+    JsonFormat.instance[Symbol](s => JsString(s.name)) {
       case JsString(x) => Symbol(x)
       case x =>
         deserializationError("Expected Symbol as JsString, but got " + x)
     }
-  }
 }
